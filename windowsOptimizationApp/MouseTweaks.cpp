e286a24806589e98dc795aa3698292240f9c91a2
DaddyJuanito <jsklucinec@gmail.com>
2024-01-14 00:08:29 -0500

    updated comments

--- windowsOptimizationApp/MouseTweaks.cpp
+++ windowsOptimizationApp/MouseTweaks.cpp
@@ -8,24 +8,26 @@ void MouseTweaks::applyDataQueue(DWORD buffer)
     // [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\mouclass\Parameters]
     // Make sure key is created and accessible.
     RegistryManager regManager(HKEY_LOCAL_MACHINE);
-    regManager.createKey("SYSTEM\\CurrentControlSet\\Services\\mouclass\\Parameters\\test");
+    regManager.createKey("SYSTEM\\CurrentControlSet\\Services\\mouclass\\Parameters");
 
     // Set values
-    regManager.createVal("SYSTEM\\CurrentControlSet\\Services\\mouclass\\Parameters\\test", buffer,
+    regManager.createVal("SYSTEM\\CurrentControlSet\\Services\\mouclass\\Parameters", buffer,
         "MouseDataQueueSize");
-    regManager.createVal("SYSTEM\\CurrentControlSet\\Services\\mouclass\\Parameters\\test", DWORD(31),
+    regManager.createVal("SYSTEM\\CurrentControlSet\\Services\\mouclass\\Parameters", DWORD(31),
         "ThreadPriority");
 }
 
 void MouseTweaks::applyBoostCsrss()
 {
+    // [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\\Windows NT\CurrentVersion\Image File Execution Options\csrss.exe\PerfOptions]
+    // Make sure key is created and accessible.
     RegistryManager regManager(HKEY_LOCAL_MACHINE);
-    regManager.createKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\csrss.exe\\PerfOptions\\test");
+    regManager.createKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\csrss.exe\\PerfOptions");
 
     // Set values
-    regManager.createVal("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\csrss.exe\\PerfOptions\\test", DWORD(4),
+    regManager.createVal("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\csrss.exe\\PerfOptions", DWORD(4),
         "CpuPriorityClass");
-    regManager.createVal("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\csrss.exe\\PerfOptions\\test", DWORD(3),
+    regManager.createVal("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\csrss.exe\\PerfOptions", DWORD(3),
         "IoPriority");
 }
 
@@ -34,7 +36,7 @@ void MouseTweaks::revertDataQueue()
     RegistryManager regManager(HKEY_LOCAL_MACHINE);
 
     // Delete keys and values to revert data queue tweak
-    regManager.deleteKey("SYSTEM\\CurrentControlSet\\Services\\mouclass\\Parameters\\test");
+    regManager.deleteKey("SYSTEM\\CurrentControlSet\\Services\\mouclass\\Parameters");
 }
 
 void MouseTweaks::revertBoostCsrss()
@@ -42,5 +44,5 @@ void MouseTweaks::revertBoostCsrss()
     RegistryManager regManager(HKEY_LOCAL_MACHINE);
 
     // Delete keys and values to revert csrss tweak
-    regManager.deleteKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\csrss.exe\\PerfOptions\\test");
+    regManager.deleteKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\csrss.exe\\PerfOptions");
 }
